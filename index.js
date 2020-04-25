cca12532f0db79832976e5f163883feb37223b0d
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-25 22:50:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,4 @@ function appendKitten(name) {
   let cats = kittens.concat(name)
   return cats
 }
+/* concat() is used to merge two or more arrays. It does not modify the original and returns a new array instead
